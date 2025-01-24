2c98d6f207c667895b5a892827e31657ff943d56
Bartłomiej Sadza <165947271+BartlomiejSadza@users.noreply.github.com>
2025-01-24 23:56:18 +0000

    now it'll work

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ class FlaskApp:
         self.app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 flask_app = FlaskApp()
+app = flask_app.app
 
 if __name__ == "__main__":
     flask_app.run()
